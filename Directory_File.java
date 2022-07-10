fee1899ed5d3d0dd26b2e4592b8fc62da32b3f41
yusooo <yusaei1107@gmail.com>
2022-07-10 23:36:38 +0900

    20220710

---
+++ Directory_File.java
@@ -0,0 +1,9 @@
+// 개발자의 파일 개발 : 모듈-src/main 소속 파일
+
+// AndroidManifest.xml : 앱의 메인 환경 파일
+// java/MainActivity.java : 화면 구성을 위한 액티비티 컴포넌트 ; 실제 MainActivity 파일이 수행되어 화면에 UI 출력
+// res : 앱의 모든 리소스 파일의 상위 폴더
+// res/drawable : 리소스 중 이미지 파일을 저장하기 위한 폴더
+// res/layout : 리소스 중 UI 구성을 위한 레이아웃 XML 파일을 위한 폴더
+// res/mipmap : 리소스 중 앱의 아이콘 이미지를 위한 폴더
+// res/values : 리소스 중 문자열 값 등을 위한 폴더
